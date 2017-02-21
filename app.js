72e6c15a2a43cadb2950f2e5b9c2baa29b2b0dde
Luying Wen <shelleying421@gmail.com>
2017-02-20 23:30:12 -0500

    Add unit test

--- app.js
+++ app.js
@@ -483,3 +483,16 @@ function saltHashPassword(userpassword) {
     // console.log('Passwordhash = '+passwordData.passwordHash);
     // console.log('nSalt = '+passwordData.salt);
 }
+
+function isAuthenticated(req, res, next) {
+
+    // do any checks you want to in here
+
+    // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
+    // you can do this however you want with whatever variables you set up
+    if (req.user.authenticated)
+        return next();
+
+    // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SOMEWHERE
+    res.redirect('/');
+}
