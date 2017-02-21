45581ec2921f209b499dbfaa952846d5d832ef51
lwen <lwen@transfinder.com>
2017-02-21 13:27:55 -0500

    Read me

--- app.js
+++ app.js
@@ -488,7 +488,6 @@ function isAuthenticated(req, res, next) {
 
     // do any checks you want to in here
 
-    // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
     // you can do this however you want with whatever variables you set up
     if (req.user.authenticated)
         return next();
